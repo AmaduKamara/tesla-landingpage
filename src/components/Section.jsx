bc0f8c8a23bc86369e2961b4a4a89fe73f460687
Amkam <amaduskamara@gmail.com>
2022-07-21 23:50:01 +0000

    Add tailwindcss

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import styled from "styled-components";
 
 function Section() {
-  return <Wrap>Section</Wrap>;
+  return (
+    <Wrap>
+      <ItemText>
+        <h1 className="text-5xl font-semibold py-2">Model S</h1>
+        <p className="text-lg">Order Online for Touchless Delivery</p>
+      </ItemText>
+    </Wrap>
+  );
 }
 
 export default Section;
@@ -10,5 +17,13 @@ export default Section;
 const Wrap = styled.div`
   width: 100vw;
   height: 100vh;
-  background-image: url()
+  background-image: url("/images/model-s.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+`;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
 `;
