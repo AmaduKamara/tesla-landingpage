a5903e0cbe388cbded36a56184bd8ef051a28f93
Amkam <amaduskamara@gmail.com>
2022-07-21 23:03:48 +0000

    Add general styles

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Tesla Clone</h1>
+      <Header />
     </div>
   );
 }
